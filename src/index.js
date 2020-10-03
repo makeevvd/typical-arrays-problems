54f803af656da989df91ab724d2a8f0eae8951a8
Vlad Makeev <makeevvd1@gmail.com>
2020-10-03 09:42:34 +0300

    init: start Typical Array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+
+  return array.reduce((prev, cur) => {
+    return cur < prev ? cur : prev
+  })
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+
+  return array.reduce((prev, cur) => {
+    return cur > prev ? cur : prev
+  })
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!array || !array.length) return 0;
+
+  let arrSum ;
+
+  arrSum = array.reduce((sum, cur) => {
+    return sum+=cur
+}, 0)
+
+  return arrSum/array.length
+  }
+
